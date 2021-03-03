d5143b97c4987aa369d9eb06abc9f4c7e07bc465
Agus Cahyono <aguscahyono@roomme.space>
2021-03-03 20:36:10 +0700

    Initial Commit

---
+++ WhatsApp Call/WhatsApp_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsApp_CallApp.swift
+//  WhatsApp Call
+//
+//  Created by Agus Cahyono on 03/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WhatsApp_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
